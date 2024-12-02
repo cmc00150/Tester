978acbcd656d35dba920496ba0f6f61b32becdba
cmc00150 <153867792+cmc00150@users.noreply.github.com>
2024-12-02 19:02:29 +0100

    porcentaje

--- tester.sh
+++ tester.sh
@@ -14,6 +14,10 @@ aciertos=0
 posicion=0
 total=$(wc -l $archivo | cut -d ' ' -f1)
 
+ancho=$(tput cols) # Devuelve el ancho de la termial
+printf -v barra "%$((${ancho}-2))s" # Creamos tantos espacios como ancho sea el terminal
+barra=${barra// /#} # Sustituimos esos espacios por #
+
 while IFS= read -r linea # Cogemos una linea del archivo seleccionado
 do
 	if [[ -z "$linea" ]]; then # Si la linea está vacía pasa a la siguiente
@@ -21,7 +25,11 @@ do
 	fi
 
 	((posicion++)) # Aumentamos la posición donde nos encontramos
- 
+
+	porcentaje=$(( ($posicion / ${#barra}) * $total )) # Calculamos el número de # para representar el porcentaje
+ 	printf -v barra "[%-${#barra}.${porcentaje}s]" # Alineamos a la izquierda y ponemos el resto a espacios
+  	echo "${barra// /-}" # Sustituimos los espacios por los -
+  
 	pregunta=${linea% - *} # Almacenamos la pregunta (eliminamos la cadena desde el final hasta ' - ')
 	printf -v solucion "%.1s" ${linea#* - } # Ahora la solución (desde el principio hasta el final ) y mediante printf quitamos todas las letras menos la primera
 	
