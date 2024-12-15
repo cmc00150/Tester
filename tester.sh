7983ee0b370a92812d7b2a64e6b048ab66e28c16
cmc00150 <153867792+cmc00150@users.noreply.github.com>
2024-12-15 02:27:11 +0000

    Opciones

--- tester.sh
+++ tester.sh
@@ -5,13 +5,39 @@ VERDE='\e[0;32m'
 FELIZ='\U1F604'
 TRISTE='\U1F622'
 
+function help(){
+	cat <<'EOF'
+		SINTAXIS:
+			tester.sh [OPTION]... Documento
+		
+		OPCIONES:
+			--random
+					Muestra las preguntas de forma aleatoria (default: forma ordenada).
+			
+			--time
+					Establece el tiempo de delay entre pregunta y pregunta (default: 1), debe ser mayor que 0.
+			
+			--penalty
+					Establece cada cuantos fallos restan una buena (default: 1), debe ser mayor que 1.
+EOF
+}
+
 function almacenarPreguntas()
 {
 	while IFS= read -r linea; do
-		if [ -z "$linea" ]; then continue; fi # Si la linea está vacía pasa a la siguiente
+		if [ -z "$linea" ]; then continue; fi # Si la línea está vacía pasa a la siguiente
 	
 		preguntas+=("$linea")  # Almacenamos las preguntas en el vector
-	done < ${1}
+	done < "${1}"
+}
+
+function orden() 
+{
+	if [ -z ${1} ]; then # Si el argumento no es una cadena vacía
+		seq 0 $(($total-1)) # Devuelve una lista ordenada
+	else
+		seq 0 $((${1}-1)) | shuf # Devuelve una lista desordenada
+	fi
 }
 
 function mostrarBarraPorcentaje()
@@ -63,13 +89,14 @@ function cooldown()
 {
 	tput civis # Escondemos el cursor
 	
-	local tiempo=1	
-	while [ $tiempo != 0 ]; do
-		tiempo=$(awk -v t="$tiempo" 'BEGIN {print t - 0.01}')
-		echo -ne "\rSiguiente pregunta ${tiempo} segundos."
+	local delayL=$delay
+
+	while [ $delayL != 0 ]; do
+		delayL=$(awk -v t="$delayL" 'BEGIN {print t - 0.01}')
+		echo -ne "\rSiguiente pregunta ${delayL} segundos."
 		sleep 0.0045 # Rellenamos el tiempo que nos sobra para que llegue a 0.01 cada iteración más o menos
 	done
-		
+
 	tput cnorm # Volvemos a mostrar el cursor
 }
 
@@ -79,35 +106,67 @@ function puntuacion()
 	echo "------ Puntuación ------"
 	awk -v a="$aciertos" -v b="$total" 'BEGIN {printf "Aciertos: %d - %.2f\n", a,  (a/b)*100}' # Aciertos (utilizamos awk porque tiene más precisión en la división)
 	awk -v a="$aciertos" -v b="$total" 'BEGIN {printf "Fallos: %d - %.2f\n", b-a,  ((b-a)/b)*100}' # Fallos
-	awk -v a="$aciertos" -v b="$total" 'BEGIN {printf "Aciertos si los fallos restan: %.1f - %.2f\n", a-((b-a)/4), ((a-((b-a)/4))/b)*100}' # Mostramos la punt. teniendo en cuenta la penalización
+	awk -v p="$penalizacion" -v a="$aciertos" -v b="$total" 'BEGIN {printf "Aciertos si los fallos restan: %.1f - %.2f\n", a-((b-a)/p), ((a-((b-a)/p))/b)*100}' # Mostramos la punt. teniendo en cuenta la penalización
 }
 
-if [ ${#} -eq 0 ]; then
-	echo "Error en los argumentos"
+if [[ ! -r ${!#} || ${1} =~ [0-9] || $# -eq 0 ]]; then # Si el primer argumento es un número o el ultimo no es un fichero da error
+	help
 	exit
 fi
 
 aciertos=0
 posicion=0
-total=$(wc -l < "${1}")
+total=$(wc -l < "${!#}")
+
+penalizacion=1
+delay=1
+ordenPreguntas=($( orden )) # Por defecto lo ponemos ordenada
+
 preguntas=()
+almacenarPreguntas "${!#}" # Le pasamos la pregunta (el último argumento)
 
-almacenarPreguntas ${1} # Le pasamos la pregunta
+for((arg=1; arg<$#; arg++)); do # Miramos las opciones que deben de estar antes del último documento
+	if [ -r ${!arg} ]; then # Si es un directorio pero no está en la última posición da un error
+		help
+		exit
+	fi
 
-for pregunta in "${preguntas[@]}"; do
+	siguiente=$(( $arg + 1))
 
-	tput sc # Guardamos la posición inicial del cursor antes de que se muestre la pregunta
+	case ${!arg} in
 
+		--random)
+			if [[ ${!siguiente} =~ [0-9] ]]; then help; exit; fi # Random no acepta argumentos
+			ordenPreguntas=($( orden $total ))
+		;;
+		--time)
+			if [[ ${!siguiente} =~ [^0-9] || ${!siguiente} < 0 ]]; then help; exit; fi # Debe pasarse un número y que sea mayor que 0
+			delay=${!siguiente}
+		;;
+		--penalty)
+			if [[ ${!siguiente} =~ [^0-9] || ${!siguiente} < 1 ]]; then help; exit; fi # Debe pasarse un número y que sea mayor que 1
+			penalizacion=${!siguiente}
+		;;
+		[0-9]) # Si es un número no hacemos nada
+		;;
+		*)
+			help
+			exit
+		;;
+	esac
+done
+
+for indice in ${ordenPreguntas[@]}
+do
 	((posicion++)) # Aumentamos la posición donde nos encontramos
   
 	mostrarBarraPorcentaje
 	
-	mostrarPregunta "$pregunta" # Le pasamos la pregunta
+	mostrarPregunta "${preguntas[$indice]}" # Le pasamos la pregunta
 	
 	cooldown
 	
 	tput clear # Limpiamos la pantalla
-	
 done
 
 puntuacion
